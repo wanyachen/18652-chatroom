f8186ece9da57aa4af8d6a5191ac07d5c6db8a42
heyq <heyq@heyqde-MacBook-Pro.local>
2016-08-17 22:13:17 -0700

    upload post (temp)

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -9,22 +9,18 @@ $(document).ready(function() {
 		$('.container button').on('click', userLogin);
 	} else if (bodyid == "chat") {
 		//chatroom page
-		var user = curUser.username;
 		console.log('in chatroom page, username = ' + curUser.username);	//for debug
-		$('#name-area').text(user);
-		//$('#name-area').html('You are: <span>' + user + '</span>');
-		//$('#name-area').html('You are: <span>trying</span>');
 
 		//button callback
-    		$('#sendBtn input').on('click', uploadPost);
-    		$('#logout').click(function() {
-		    console.log('logout link is clicked');
-		    window.location.href = '/';	//relative ?
-		});
+    		//$('#postbtn').on('click', uploadPost);
+    		//$('#logout').click(function() {
+		//    console.log('logout link is clicked');
+		//    window.location.href = '/';	//relative ?
+		//});
 	}
 });
 
-// callback functions
+// ---------------------- callback functions ----------------------------
 function userLogin(event) {
 	event.preventDefault();
 
@@ -53,3 +49,73 @@ function userLogin(event) {
 		}
  	});
 }
+
+// Upload post
+//function uploadPost(event) {
+function uploadPost() {
+//    event.preventDefault();
+
+    //1. save to database
+    //2. update article in page.jade
+
+    var text = $('#comment').val();
+    //var tim = Date();
+
+    var formatted = $.datepicker.formatDate("M d, yy", new Date());
+
+    //upload to server
+    // If it is, compile all user info into one object
+    var newPost = {
+        //'username': '',	//wait for server
+        'username': curUser.username,
+	//'timestamp': tim,
+	'timestamp': formatted,
+	'content': text
+    }
+
+    //for debug
+    console.log('uploadPost is triggered...');
+
+    //get username from server ??
+    $.ajax({
+        type: 'POST',
+        data: newPost,
+        url: '/chatroom/upload',
+        dataType: 'JSON',
+	statusCode : {
+		500: function(response) {
+			alert('Error: ' + response.body);
+		}
+	}, 
+	success: function(response) {
+		//console.log("user = " + response.user + ", newUser.username = " + newUser.username);	
+		console.log("user = " + response.user);	
+    		renderPost(response.user, /*tim,*/formatted, text);	//update content here
+	},
+    });
+ 
+}
+
+// ---------------------- helper functions ----------------------------
+function renderPost(username, timestamp, content) {
+
+//    var table = $('<table></table>').addClass('msgbox');
+//    var row1 = $('<tr></tr>').addClass('item').text(username).css({"background-color": "yellow", "width" : "50%"});
+//    var row2 = $('<tr></tr>').addClass('item').text(timestamp).css({"background-color": "red", "width" : "50%", "text-align": "right", "margin-top" : "0"});
+//    var row3 = $('<tr></tr>').addClass('item').text(content).css({"background-color": "lightblue", "width" : "50%", "margin-top" : "0"});
+//
+//    table.append(row1, row2, row3);
+//    $('#postthread').prepend(table);	//newest is on the top
+
+    //for debug
+    //add to fixed panel
+    //for debug
+    console.log('renderPost is triggered...');
+    //$('.panel.panel-default .panel-heading .panel-title').text(username);
+    $('.panel.panel-default .panel-heading p:first').text(username);
+    $('.panel.panel-default .panel-heading p:last').text(timestamp).css({'text-align': "right"});
+    //$('#datepicker').text(timestamp);
+    //$('#datepicker').datetimepicker({timeFormat: "hh:mm tt"});
+    $('.panel.panel-default .panel-body').text(content);
+}
+
--- routes/chatroom.js
+++ routes/chatroom.js
@@ -11,5 +11,25 @@ router.get('/', function(req, res) {
 	console.log("in chatroom, username = " + sess.username);
 });
 
+//save the post to db
+router.post('/upload', function(req, res) {
+    var db = req.db;
+    var collection = db.get('postlist');
+    //req.body : JSON data
+    //{username, timestamp, content}
+    req.body.username = req.session.username;	//update ??
+    collection.insert(req.body, function(err, result){
+	//heyq, send username to client
+	if (err === null) {
+		console.log("collection insert success: " + sess.username);
+		res.json({user: sess.username});
+	} else {
+		console.log("collection insert fail: " + sess.username);
+		res.status(500);	//internal server error
+		res.send(err);
+	}
+    });
+});
+
 
 module.exports = router;
